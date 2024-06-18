ac7f399fe8efe7a3da7962fe4296280da3e8efc1
gdjolt8 <102922030+gdjolt8@users.noreply.github.com>
2024-06-18 18:19:24 -0400

    Update main.c

--- main.c
+++ main.c
@@ -22,29 +22,35 @@ void do_command(char command, FILE *input, FILE *output) {
 
     switch (command) {
         case '>':
-            fprintf(output, "\n\tmovq -8(%%rbp), %%rax\n\taddq $1, %%rax\n\tmovq %%rax, -8(%%rbp)");
+            fprintf(output, "\n# >\n\tmovq -8(%%rbp), %%rax\n\taddq $1, %%rax\n");
+            fprintf(output, "\tleaq -8(%%rbp), %%rbx\n\taddq $32768, %%rbx\n\tcmpq %%rbx, %%rax\n\tjnb .Lmem_check_end_%d\n", label_counter);
+            fprintf(output, "\tmovq %%rbx, -8(%%rbp)\n.Lmem_check_end_%d:\n", label_counter++);
+            fprintf(output, "\tmovq %%rax, -8(%%rbp)");
             break;
         case '<':
-            fprintf(output, "\n\tmovq -8(%%rbp), %%rax\n\tsubq $1, %%rax\n\tmovq %%rax, -8(%%rbp)");
+            fprintf(output, "\n# <\n\tmovq -8(%%rbp), %%rax\n\tsubq $1, %%rax\n");
+            fprintf(output, "\tleaq -8(%%rbp), %%rbx\n\tcmpq %%rbx, %%rax\n\tjl .Lmem_check_start_%d\n", label_counter);
+            fprintf(output, "\tleaq -8(%%rbp), %%rbx\n\taddq $32767, %%rbx\n\tmovq %%rbx, -8(%%rbp)\n\t.Lmem_check_start_%d:\n", label_counter++);
+            fprintf(output, "\tmovq %%rax, -8(%%rbp)");
             break;
         case '+':
-            fprintf(output, "\n\tmovq -8(%%rbp), %%rax\n\tmovzbl (%%rax), %%edx\n\taddl $1, %%edx\n\tmovb %%dl, (%%rax)");
+            fprintf(output, "\n# +\n\tmovq -8(%%rbp), %%rax\n\tmovzbl (%%rax), %%edx\n\taddl $1, %%edx\n\tmovb %%dl, (%%rax)");
             break;
         case '-':
-            fprintf(output, "\n\tmovq -8(%%rbp), %%rax\n\tmovzbl (%%rax), %%edx\n\tsubl $1, %%edx\n\tmovb %%dl, (%%rax)");
+            fprintf(output, "\n# -\n\tmovq -8(%%rbp), %%rax\n\tmovzbl (%%rax), %%edx\n\tsubl $1, %%edx\n\tmovb %%dl, (%%rax)");
             break;
         case '.':
-            fprintf(output, "\n\tmovq -8(%%rbp), %%rax\n\tmovzbl (%%rax), %%eax\n\tmovsbl %%al, %%eax\n\tmovl %%eax, %%edi\n\tcall putchar");
+            fprintf(output, "\n# .\n\tmovq -8(%%rbp), %%rax\n\tmovzbl (%%rax), %%eax\n\tmovzbl %%al, %%eax\n\tmovl %%eax, %%edi\n\tcall putchar");
             break;
         case ',':
-            fprintf(output, "\n\tcall getchar\n\tmovl %%eax, %%edx\n\tmovq -8(%%rbp), %%rax\n\tmovb %%dl, (%%rax)");
+            fprintf(output, "\n# ,\n\tcall getchar\n\tmovl %%eax, %%edx\n\tmovq -8(%%rbp), %%rax\n\tmovb %%dl, (%%rax)");
             break;
         case '[': {
             char *start_label = generate_label();
             char *end_label = generate_label();
 
-            fprintf(output, "\n%s:", start_label);
-            fprintf(output, "\n\tmovq -8(%%rbp), %%rax\n\tmovzbl (%%rax), %%eax\n\ttestb %%al, %%al\n\tjz %s", end_label);
+            fprintf(output, "\n# [\n%s:", start_label);
+            fprintf(output, "\n\tmovq -8(%%rbp), %%rax\n\tmovzbl (%%rax), %%eax\n\ttestb %%al, %%al\n\tje %s", end_label);
 
             while ((c = getc(input)) != EOF && c != ']') {
                 do_command(c, input, output);
@@ -74,7 +80,11 @@ char* find_flags(int argc, char **args) {
                 return args[i+1];
             }
         }
+        if (strcmp(args[i], "-S") == 0) {
+            return "s";
+        }
     }
+    return NULL;
 }
 
 int main(int argc, char **argv) {
@@ -88,6 +98,7 @@ int main(int argc, char **argv) {
     const char *output_file_name;
     FILE *input;
     FILE *output = fopen("output.s", "wb");
+    unsigned int S = 0;
     if (!output) {
         perror("fopen");
         return 1;
@@ -100,11 +111,15 @@ int main(int argc, char **argv) {
                 fprintf(stderr, "Unable to open \"%s\"\n", argv[1]);
                 exit(EXIT_FAILURE);
         }
+        if (find_flags(argc, argv) != NULL) {
+            char *flags = find_flags(argc, argv);
+            if (flags[0] == 's') S++;
+        }
     } else {
         fprintf(stderr, "Usage: %s <input.bf>\n", argv[0]);
         return 0;
     }
-    fprintf(output, ".section .text\n.global main\nmain:\n\tpushq %%rbp\n\tmovq %%rsp, %%rbp\n\tsubq $30000, %%rsp\n\tleaq -30000(%%rbp), %%rax\n\tmovl $30000, %%edx\n\tmovl $0, %%esi\n\tmovq %%rax, %%rdi\n\tcall memset\n\tleaq -30000(%%rbp), %%rax\n\tmovq %%rax, -8(%%rbp)\n");
+    fprintf(output, ".section .text\n.global main\nmain:\n\tpushq %%rbp\n\tmovq %%rsp, %%rbp\n\tsubq $32792, %%rsp\n\tleaq -32768(%%rbp), %%rax\n\tmovl $32768, %%edx\n\tmovl $0, %%esi\n\tmovq %%rax, %%rdi\n\tcall memset\n\tleaq -32768(%%rbp), %%rax\n\tmovq %%rax, -8(%%rbp)\n");
 
     while ((command = getc(input)) != EOF) {
         do_command(command, input, output);
@@ -118,7 +133,13 @@ int main(int argc, char **argv) {
     char *cmd = (char *)calloc(1024, sizeof(char));
     sprintf(cmd, "gcc output.s -no-pie -w -Wl,-z,noexecstack -o %s\n", output_file_name);
     system(cmd);
-    system("rm output.s");
     free(cmd);
+
+    FILE *f = fopen("output.s", "rb");
+    fseek(f, 0, SEEK_END);
+    long l = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    printf("Generated %s!\nSize: %ld bytes\n", output_file_name, l);
+    if (!S) system("rm output.s");
     return 0;
 }
